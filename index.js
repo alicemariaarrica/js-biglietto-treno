cfeff6c93fa3dc46b92b18ee95f9ce6089a3c834
alicemariaarrica <alicemariaarrica@gmail.com>
2024-09-30 14:42:06 +0200

    var-assignement-commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 
-let numKm;  //var di prompt//
+let numKm;  //km da moltiplicare per 0,21 Euro (&usare prompt)//
 
-let numEtà; //var di prompt//
+let numEtà; //anni del passeggero (&usare prompt)//
 
+let prezzo; //prezzo del biglietto da definire//
 
+let scontoMinorenni; //percentuale da sottrarre al prezzo definito solo per chi ha l'età<18//
 
+let scontoAnziani; //percentuale da sottrarre al prezzo definito solo per chi ha l'età>65//
 
 
 
